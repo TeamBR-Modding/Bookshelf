a8c70a43ea679a4033e2c041377ea161e34e1057
Paul Davis <pauldavis101@gmail.com>
2015-07-03 23:07:47 -0500

    Forge rotation utils

---
+++ src/main/java/com/dyonovan/brlib/util/WorldUtils.java
@@ -0,0 +1,50 @@
+package com.dyonovan.brlib.util;
+
+import net.minecraftforge.common.util.ForgeDirection;
+
+public class WorldUtils {
+
+    /**
+     * Returns the direction to the left of this. This is the direction it is facing turned left
+     * @param toTurn Starting point
+     * @return The direction turned 90 left
+     */
+    public ForgeDirection rotateLeft(ForgeDirection toTurn) {
+        switch(toTurn) {
+            case NORTH :
+                return ForgeDirection.WEST;
+            case EAST :
+                return ForgeDirection.NORTH;
+            case SOUTH :
+                return ForgeDirection.EAST;
+            case WEST :
+                return ForgeDirection.SOUTH;
+            case UP :
+            case DOWN :
+            default :
+                return toTurn;
+        }
+    }
+
+    /**
+     * Returns the direction to the right of this. This is the direction it is facing turned right
+     * @param toTurn Starting point
+     * @return The direction turned 90 right
+     */
+    public ForgeDirection rotateRight(ForgeDirection toTurn) {
+        switch(toTurn) {
+            case NORTH :
+                return ForgeDirection.EAST;
+            case EAST :
+                return ForgeDirection.SOUTH;
+            case SOUTH :
+                return ForgeDirection.WEST;
+            case WEST :
+                return ForgeDirection.NORTH;
+            case UP :
+            case DOWN :
+            default :
+                return toTurn;
+        }
+    }
+}
