ff83d2e4297ac3d4286f849fea5deee4e85e8fde
Dyonovan <mcbetacraft@gmail.com>
2015-07-09 21:42:39 -0400

    Added Waila Comms

--- src/main/java/com/teambr/bookshelf/client/ClientProxy.java
+++ src/main/java/com/teambr/bookshelf/client/ClientProxy.java
@@ -3,6 +3,7 @@ package com.teambr.bookshelf.client;
 import com.teambr.bookshelf.client.renderer.BasicBlockRenderer;
 import com.teambr.bookshelf.common.CommonProxy;
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.common.event.FMLInterModComms;
 
 public class ClientProxy extends CommonProxy {
     public static int renderPass;
@@ -10,5 +11,9 @@ public class ClientProxy extends CommonProxy {
     public void init() {
         BasicBlockRenderer.renderID = RenderingRegistry.getNextAvailableRenderId();
         RenderingRegistry.registerBlockHandler(new BasicBlockRenderer());
+
+        //Register with Waila
+        FMLInterModComms.sendMessage("Waila", "register",
+                "com.teambr.bookshelf.api.waila.WailaDataProvider.callbackRegister");
     }
 }
