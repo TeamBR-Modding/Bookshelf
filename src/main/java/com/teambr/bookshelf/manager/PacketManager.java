8135ad26949affa754abf953796016e4520798ae
Paul Davis <pauldavis101@gmail.com>
2015-07-10 21:37:46 -0500

    Send client pack with one call

--- src/main/java/com/teambr/bookshelf/manager/PacketManager.java
+++ src/main/java/com/teambr/bookshelf/manager/PacketManager.java
@@ -5,6 +5,8 @@ import com.teambr.bookshelf.network.ClientTileUpdate;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.relauncher.Side;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
 
 public class PacketManager {
     public static SimpleNetworkWrapper net;
@@ -17,9 +19,16 @@ public class PacketManager {
     private static int nextPacketId = 0;
 
     @SuppressWarnings("unchecked")
-    private static void registerMessage(Class packet, Class message)
-    {   net.registerMessage(packet, message, nextPacketId, Side.CLIENT);
+    private static void registerMessage(Class packet, Class message) {
+        net.registerMessage(packet, message, nextPacketId, Side.CLIENT);
         net.registerMessage(packet, message, nextPacketId, Side.SERVER);
         nextPacketId++;
+    }
+
+    public static void updateTileWithClientInfo(TileEntity tile) {
+        NBTTagCompound tag = new NBTTagCompound();
+        tile.writeToNBT(tag);
+        ClientTileUpdate.Message updateMessage = new ClientTileUpdate.Message(tile.xCoord, tile.yCoord, tile.zCoord, tag);
+        net.sendToServer(updateMessage);
     }
 }
