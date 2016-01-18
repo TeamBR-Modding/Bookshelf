65cebbf929d9ce1060ba906910e75e8962b3b2de
Paul Davis <pauldavis101@gmail.com>
2016-01-18 17:01:58 -0600

    Out of bounds check

--- src/main/scala/com/teambr/bookshelf/common/tiles/traits/Inventory.scala
+++ src/main/scala/com/teambr/bookshelf/common/tiles/traits/Inventory.scala
@@ -13,15 +13,15 @@ import net.minecraft.util.{ ChatComponentText, IChatComponent, StatCollector }
 import scala.collection.mutable.ArrayBuffer
 
 /**
- * This file was created for Bookshelf
- *
- * Bookshelf is licensed under the
- * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License:
- * http://creativecommons.org/licenses/by-nc-sa/4.0/
- *
- * @author Paul Davis pauljoda
- * @since August 03, 2015
- */
+  * This file was created for Bookshelf
+  *
+  * Bookshelf is licensed under the
+  * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License:
+  * http://creativecommons.org/licenses/by-nc-sa/4.0/
+  *
+  * @author Paul Davis pauljoda
+  * @since August 03, 2015
+  */
 trait Inventory extends IInventory with NBTSavable {
 
     var inventoryName : String
@@ -33,44 +33,44 @@ trait Inventory extends IInventory with NBTSavable {
 
 
     /**
-     * Used to add a callback to this inventory
-     * @param iInventory What you want to be called
-     * @return This inventory
-     */
+      * Used to add a callback to this inventory
+      * @param iInventory What you want to be called
+      * @return This inventory
+      */
     def addCallback(iInventory: IInventoryCallback) : Inventory = {
         callBacks += iInventory
         this
     }
 
     /**
-     * Called when the inventory has a change
-     * @param slot The slot that changed
-     */
+      * Called when the inventory has a change
+      * @param slot The slot that changed
+      */
     def onInventoryChanged(slot : Int) = callBacks.foreach((callBack : IInventoryCallback) => callBack.onInventoryChanged(this, slot))
 
     /**
-     * Used to add just one stack into the end of the inventory
-     * @param stack The stack to push
-     */
+      * Used to add just one stack into the end of the inventory
+      * @param stack The stack to push
+      */
     def addInventorySlot(stack : ItemStack) = inventoryContents.push(stack)
 
     /**
-     * Used to push x amount of slots into the inventory
-     * @param count How many slots to add
-     */
+      * Used to push x amount of slots into the inventory
+      * @param count How many slots to add
+      */
     def addInventorySlots(count : Int) = for(i <- 0 until count) addInventorySlot(null)
 
     /**
-     * Used to remove the last element of the stack
-     * @return The last stack, now popped
-     */
+      * Used to remove the last element of the stack
+      * @return The last stack, now popped
+      */
     def removeInventorySlot() : ItemStack = inventoryContents.pop()
 
     /**
-     * Used to remove a specific amount of items
-     * @param count The count of slots to remove
-     * @return The array of the stacks that were there
-     */
+      * Used to remove a specific amount of items
+      * @param count The count of slots to remove
+      * @return The array of the stacks that were there
+      */
     def removeInventorySlots(count : Int) : Array[ItemStack] = {
         val poppedStack = new Array[ItemStack](count)
         for(i <- 0 until count) poppedStack(i) = removeInventorySlot()
@@ -78,9 +78,9 @@ trait Inventory extends IInventory with NBTSavable {
     }
 
     /**
-     * Used to copy from an existing inventory
-     * @param inventory The inventory to copy from
-     */
+      * Used to copy from an existing inventory
+      * @param inventory The inventory to copy from
+      */
     def copyFrom(inventory : IInventory) = {
         for(i <- 0 until inventory.getSizeInventory) {
             if (i < getSizeInventory) {
@@ -91,16 +91,16 @@ trait Inventory extends IInventory with NBTSavable {
     }
 
     /**
-     * Used to save the inventory to an NBT tag
-     * @param tag The tag to save to
-     */
+      * Used to save the inventory to an NBT tag
+      * @param tag The tag to save to
+      */
     def writeToNBT(tag : NBTTagCompound) : Unit = writeToNBT(tag, "")
 
     /**
-     * Used to save the inventory to an NBT tag
-     * @param tag The tag to save to
-     * @param inventoryName The name, in case you have more than one
-     */
+      * Used to save the inventory to an NBT tag
+      * @param tag The tag to save to
+      * @param inventoryName The name, in case you have more than one
+      */
     def writeToNBT(tag : NBTTagCompound, inventoryName : String) = {
         tag.setInteger("Size:" + inventoryName, getSizeInventory)
         val nbttaglist = new NBTTagList
@@ -116,16 +116,16 @@ trait Inventory extends IInventory with NBTSavable {
     }
 
     /**
-     * Used to read the inventory from an NBT tag compound
-     * @param tag The tag to read from
-     */
+      * Used to read the inventory from an NBT tag compound
+      * @param tag The tag to read from
+      */
     def readFromNBT(tag : NBTTagCompound) : Unit = readFromNBT(tag, "")
-    
+
     /**
-     * Used to read the inventory from an NBT tag compound
-     * @param tag The tag to read from
-     * @param inventoryName The inventory name
-     */
+      * Used to read the inventory from an NBT tag compound
+      * @param tag The tag to read from
+      * @param inventoryName The inventory name
+      */
     def readFromNBT(tag : NBTTagCompound, inventoryName : String) = {
         val nbttaglist = tag.getTagList("Items:" + inventoryName, 10)
         inventoryContents = new util.Stack[ItemStack]
@@ -143,11 +143,11 @@ trait Inventory extends IInventory with NBTSavable {
       *******************************************************************************************************************/
 
     /**
-     * Called to decrease a stack in the inventory
-     * @param slot The slot to decrease
-     * @param amount The amount to remove
-     * @return The new stack (the one picked up)
-     */
+      * Called to decrease a stack in the inventory
+      * @param slot The slot to decrease
+      * @param amount The amount to remove
+      * @return The new stack (the one picked up)
+      */
     override def decrStackSize(slot : Int, amount : Int) : ItemStack = {
         if(inventoryContents.get(slot) != null) {
             var stack : ItemStack = null
@@ -169,25 +169,30 @@ trait Inventory extends IInventory with NBTSavable {
         }
         null
     }
-    
+
     /**
-     * Gets the stack limit of the inventory
-     * @return How big this can stack
-     */
+      * Gets the stack limit of the inventory
+      * @return How big this can stack
+      */
     override def getInventoryStackLimit: Int = 64
 
     /**
-     * Get the size of this inventory
-     * @return How big this is
-     */
+      * Get the size of this inventory
+      * @return How big this is
+      */
     override def getSizeInventory: Int = inventoryContents.size()
 
     /**
-     * Get the stack in the given slot
-     * @param index The slot id
-     * @return THe stack in the slot
-     */
-    override def getStackInSlot(index: Int): ItemStack = inventoryContents.get(index)
+      * Get the stack in the given slot
+      * @param index The slot id
+      * @return THe stack in the slot
+      */
+    override def getStackInSlot(index: Int): ItemStack = {
+        if(index < inventoryContents.size())
+            inventoryContents.get(index)
+        else
+            null
+    }
 
     /**
       * Removes a stack from the given slot and returns it.
@@ -199,10 +204,10 @@ trait Inventory extends IInventory with NBTSavable {
     }
 
     /**
-     * Used to get the stack when closing. Used when you drop items on the floor ie a workbench
-     * @param index The slot
-     * @return What was in that stack, now null
-     */
+      * Used to get the stack when closing. Used when you drop items on the floor ie a workbench
+      * @param index The slot
+      * @return What was in that stack, now null
+      */
     def getStackInSlotOnClosing(index: Int): ItemStack = {
         if(index >= this.inventoryContents.size()) return null
         if(this.inventoryContents.get(index) != null) {
@@ -214,25 +219,25 @@ trait Inventory extends IInventory with NBTSavable {
     }
 
     /**
-     * Used to define if an item is valid for a slot
-     * @param index The slot id
-     * @param stack The stack to check
-     * @return True if you can put this there
-     */
+      * Used to define if an item is valid for a slot
+      * @param index The slot id
+      * @param stack The stack to check
+      * @return True if you can put this there
+      */
     override def isItemValidForSlot(index: Int, stack: ItemStack): Boolean = true
 
     /**
-     * Used to see if the player can interact with this inventory
-     * @param player The player trying to access
-     * @return True if the player is allowed
-     */
+      * Used to see if the player can interact with this inventory
+      * @param player The player trying to access
+      * @return True if the player is allowed
+      */
     override def isUseableByPlayer(player: EntityPlayer): Boolean = true
 
     /**
-     * Set the slot contents
-     * @param index The index
-     * @param stack The set to set
-     */
+      * Set the slot contents
+      * @param index The index
+      * @param stack The set to set
+      */
     override def setInventorySlotContents(index: Int, stack: ItemStack): Unit = {
         this.inventoryContents.set(index, stack)
 
@@ -243,43 +248,43 @@ trait Inventory extends IInventory with NBTSavable {
     }
 
     /**
-     * Going to go ahead and implement this, but don't trust it to update. Minecraft won't call enough to really follow
-     */
+      * Going to go ahead and implement this, but don't trust it to update. Minecraft won't call enough to really follow
+      */
     override def markDirty(): Unit = onInventoryChanged(0)
 
     /**
-     * Delete all the things
-     */
+      * Delete all the things
+      */
     override def clear(): Unit = for(i <- 0 until inventoryContents.size) inventoryContents.set(i, null)
 
     /**
-     * Something new in 1.8. Might allow you to edit things from client
-     */
+      * Something new in 1.8. Might allow you to edit things from client
+      */
     override def getFieldCount: Int = 0
 
     /**
-     * Something new in 1.8. Might allow you to edit things from client
-     */
+      * Something new in 1.8. Might allow you to edit things from client
+      */
     override def getField(id: Int): Int = 0
 
     /**
-     * Something new in 1.8. Might allow you to edit things from client
-     */
+      * Something new in 1.8. Might allow you to edit things from client
+      */
     override def setField(id: Int, value: Int): Unit = {}
 
     /**
-     * Called when the player opens the inventory.
-     *
-     * An example would be the chest, signaling the render to move the lid
-     * @param player The player
-     */
+      * Called when the player opens the inventory.
+      *
+      * An example would be the chest, signaling the render to move the lid
+      * @param player The player
+      */
     override def openInventory(player: EntityPlayer): Unit = {}
 
     /**
-     * Called when the player closes the inventory.
-     *
-     * An example would be the chest, signaling the render to move the lid
-     */
+      * Called when the player closes the inventory.
+      *
+      * An example would be the chest, signaling the render to move the lid
+      */
     override def closeInventory(player: EntityPlayer): Unit = {}
 
 
@@ -288,20 +293,20 @@ trait Inventory extends IInventory with NBTSavable {
       ********************************************************************************************************************/
 
     /**
-     * New in 1.8, gives a name to the inventory
-     * @return The name
-     */
+      * New in 1.8, gives a name to the inventory
+      * @return The name
+      */
     override def getName: String = inventoryName
 
     /**
-     * Does this inventory has a custom name
-     * @return True if there is a name (localized)
-     */
+      * Does this inventory has a custom name
+      * @return True if there is a name (localized)
+      */
     override def hasCustomName: Boolean
 
     /**
-     * New in 1.8, gives a name to the inventory. Probably used in chat messages
-     * @return The name
-     */
+      * New in 1.8, gives a name to the inventory. Probably used in chat messages
+      * @return The name
+      */
     override def getDisplayName: IChatComponent = new ChatComponentText(if(hasCustomName) StatCollector.translateToLocal(inventoryName) else "Generic Inventory")
 }
