5432e3cb7df6b83bc67e0db10ac5b2965e3df053
Paul Davis <pauldavis101@gmail.com>
2016-01-27 13:09:04 -0600

    Revert "Revert "Its clear I'm getting tired""
    This reverts commit 8d82c6e1a6bf27693058c333ae93762a04fdf2df.

--- src/main/scala/com/teambr/bookshelf/util/InventoryUtils.scala
+++ src/main/scala/com/teambr/bookshelf/util/InventoryUtils.scala
@@ -68,8 +68,8 @@ object InventoryUtils {
 
         for(x <- 0 until fromSlots.size) {
             if(fromInventory.getStackInSlot(fromSlots.get(x)) != null) {
-                for (x <- 0 until toSlots.size()) {
-                    val slotID = toSlots.get(x)
+                for (j <- 0 until toSlots.size()) {
+                    val slotID = toSlots.get(j)
                     if (!ItemStack.areItemStacksEqual(fromInventory.getStackInSlot(fromSlots.get(x)).copy(), otherInv.insertItem(slotID, fromInventory.extractItem(fromSlots.get(x), maxAmount, !doMove), !doMove)))
                         return true
                 }
