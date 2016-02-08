39fcd928291431b936cde663913c4bc21bc7a7c1
Paul Davis <pauldavis101@gmail.com>
2016-02-07 19:08:22 -0600

    New Utils

--- src/main/scala/com/teambr/bookshelf/util/InventoryUtils.scala
+++ src/main/scala/com/teambr/bookshelf/util/InventoryUtils.scala
@@ -117,6 +117,87 @@ object InventoryUtils {
         false //Nothing was moved
     }
 
+    def getStackLeftAfterMove(source : AnyRef, fromSlot : Int, target : AnyRef, intoSlot : Int, maxAmount : Int, dir : EnumFacing, doMove : Boolean, checkSidedSource : Boolean = true, checkSidedTarget : Boolean = true) : Option[ItemStack] = {
+        //Try to cast source
+        var fromInventory : IItemHandler = null
+
+        if(!source.isInstanceOf[IItemHandler]) {
+            source match {
+                case iInventory: IInventory if !iInventory.isInstanceOf[ISidedInventory] => fromInventory = new InvWrapper(iInventory)
+                case iSided: ISidedInventory => fromInventory = new SidedInvWrapper(iSided, dir.getOpposite)
+                case _ => return None //Not an inventory or IItemHandler
+            }
+        } else source match { //If we are a ItemHandler, we want to make sure not to wrap, it can be both IInventory and IItemHandler
+            case itemHandler: IItemHandler => fromInventory = itemHandler
+            case _ => return None //Nothing else, somehow?
+        }
+
+        if(checkSidedSource) {
+            source match { //Check for sidedness
+                case tileEntity: TileEntity if tileEntity.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, dir) =>
+                    fromInventory = tileEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, dir)
+                case _ =>
+            }
+        }
+
+        //Try to case sink
+        var otherInv : IItemHandler = null
+
+        if(!target.isInstanceOf[IItemHandler]) {
+            target match {
+                case iInventory: IInventory if !iInventory.isInstanceOf[ISidedInventory] => otherInv = new InvWrapper(iInventory)
+                case iSided: ISidedInventory => otherInv = new SidedInvWrapper(iSided, dir.getOpposite)
+                case _ => return None //Not an inventory or IItemHandler
+            }
+        } else target match { //If we are a ItemHandler, we want to make sure not to wrap, it can be both IInventory and IItemHandler
+            case itemHandler: IItemHandler => otherInv = itemHandler
+            case _ => return None //Nothing else, somehow?
+        }
+
+        if(checkSidedTarget) {
+            target match { //Check for sidedness
+                case tileEntity: TileEntity if tileEntity.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, dir.getOpposite) =>
+                    otherInv = tileEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, dir.getOpposite)
+                case _ =>
+            }
+        }
+
+        val fromSlots = new util.ArrayList[Int]()
+        //Add from slots
+        if(fromSlot != -1)
+            fromSlots.add(fromSlot)
+        else
+            for(x <- 0 until fromInventory.getSlots)
+                fromSlots.add(x)
+
+        val toSlots = new util.ArrayList[Int]()
+        //Add to slots
+        if(intoSlot != -1)
+            toSlots.add(intoSlot)
+        else
+            for(x <- 0 until otherInv.getSlots)
+                toSlots.add(x)
+
+        //Do actual Movement
+        for(x <- 0 until fromSlots.size) { //Cycle the from inventory
+            if(fromInventory.getStackInSlot(fromSlots.get(x)) != null) { //If something does exist
+            val fromStack = fromInventory.extractItem(fromSlots.get(x), maxAmount, true) //Simulate so we can see changes
+                if (fromStack != null) { //Make sure something was extracted
+                    for (j <- 0 until toSlots.size()) { //Try to put it somewhere
+                    val slotID = toSlots.get(j) //Get the slot to put into
+                    val movedStack = otherInv.insertItem(slotID, fromStack.copy(), !doMove) //Try to insert
+                        if (!ItemStack.areItemStacksEqual(fromStack, movedStack)) { //If the insert changed the stack
+                            fromInventory.extractItem(fromSlots.get(x),
+                                if (movedStack != null) fromStack.stackSize - movedStack.stackSize else maxAmount, !doMove) //Do Extraction on original
+                            return Some(movedStack.copy()) //We did it!
+                        }
+                    }
+                }
+            }
+        }
+        None //Nothing was moved
+    }
+
     def tryMergeStacks(stackToMerge: ItemStack, stackInSlot: ItemStack): Boolean = {
         if (stackInSlot == null || !stackInSlot.isItemEqual(stackToMerge) || !ItemStack.areItemStackTagsEqual(stackToMerge, stackInSlot)) return false
         val newStackSize: Int = stackInSlot.stackSize + stackToMerge.stackSize
