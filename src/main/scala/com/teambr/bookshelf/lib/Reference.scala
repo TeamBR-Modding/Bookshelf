e4a1a61f85e555f738e867af36f685f91dd98916
Dyonovan <lookinforfun0000@gmail.com>
2016-03-31 12:17:41 -0400

    Version bump for release

--- src/main/scala/com/teambr/bookshelf/lib/Reference.scala
+++ src/main/scala/com/teambr/bookshelf/lib/Reference.scala
@@ -13,7 +13,7 @@ package com.teambr.bookshelf.lib
 object Reference {
     final val MODID          = "bookshelfapi"
     final val MODNAME        = "Bookshelf"
-    final val VERSION        = "3.0.2"//@VERSION@"
+    final val VERSION        = "3.0.3"//@VERSION@"
     final val DEPENDENCIES   = "required-after:Forge@[11.16.0.1811,)"
     final val DEBUG          = "Debug Mode"
 }
