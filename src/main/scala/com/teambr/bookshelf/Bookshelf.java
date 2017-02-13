9a525732b0d1198fcefc04b32a397afe48850e2f
Paul Davis <pauldavis101@gmail.com>
2017-02-12 18:37:29 -0600

    Werid Refaction erros

--- src/main/scala/com/teambr/bookshelf/Bookshelf.java
+++ src/main/scala/com/teambr/bookshelf/Bookshelf.java
@@ -18,7 +18,7 @@ import java.io.File;
 /**
  * This file was created for com.teambr.bookshelf.Bookshelf - Java
  * <p>
- * com.teambr.bookshelf.Bookshelf - Java is licensed under the
+ * Bookshelf - Java is licensed under the
  * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License:
  * http://creativecommons.org/licenses/by-nc-sa/4.0/
  *
