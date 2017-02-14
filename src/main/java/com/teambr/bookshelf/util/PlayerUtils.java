c94927c1e1093a1dc56e267435214b2625344e60
Paul Davis <pauldavis101@gmail.com>
2017-02-14 17:28:38 -0600

    Porting useful classes

---
+++ src/main/java/com/teambr/bookshelf/util/PlayerUtils.java
@@ -0,0 +1,46 @@
+package com.teambr.bookshelf.util;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
+
+/**
+ * This file was created for Bookshelf
+ * <p>
+ * Bookshelf is licensed under the
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License:
+ * http://creativecommons.org/licenses/by-nc-sa/4.0/
+ *
+ * @author Paul Davis - pauljoda
+ * @since 2/14/2017
+ */
+public class PlayerUtils {
+
+    /**
+     * Checks if the player is holding the given item in either hand
+     * @param player The player entity
+     * @param item The item to check
+     * @return True if either hand contains the item
+     */
+    public static boolean isPlaylerHoldingEither(EntityPlayer player, Item item) {
+        return !(player == null || item == null || player.getHeldEquipment() == null) &&
+                ((player.getHeldItemMainhand() != null && player.getHeldItemMainhand().getItem() == item) ||
+                        (player.getHeldItemOffhand() != null && player.getHeldItemOffhand().getItem() == item));
+    }
+
+    /**
+     * Gets what hand this item is in, this does an object match so you must send the object, not match by values
+     * @param stack The object
+     * @return What hand its in
+     */
+    public static EnumHand getHandStackIsIn(EntityPlayer player, ItemStack stack) {
+        if(player == null || stack == null)
+            return EnumHand.MAIN_HAND;
+
+        if(player.getHeldItemMainhand() != null && player.getHeldItemMainhand().equals(stack))
+            return EnumHand.MAIN_HAND;
+        else
+            return EnumHand.OFF_HAND;
+    }
+}
---
+++ src/main/java/com/teambr/bookshelf/util/TimeUtils.java
@@ -0,0 +1,79 @@
+package com.teambr.bookshelf.util;
+
+import com.google.common.eventbus.Subscribe;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+/**
+ * This file was created for Bookshelf
+ * <p>
+ * Bookshelf is licensed under the
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License:
+ * http://creativecommons.org/licenses/by-nc-sa/4.0/
+ *
+ * @author Paul Davis - pauljoda
+ * @since 2/14/2017
+ */
+public class TimeUtils {
+    public static long tick = 0;
+
+    /**
+     * The seconds online
+     *
+     * @return How many seconds on, floored
+     */
+    public static int seconds() {
+        return (int) Math.floor(tick / 20);
+    }
+
+    /**
+     * How many minutes on
+     *
+     * @return Minutes, floored
+     */
+    public static int minutes() {
+        return (int) Math.floor(seconds() / 60);
+    }
+
+    /**
+     * How many hours on
+     *
+     * @return Hours, floored
+     */
+    public static int hours() {
+        return (int) Math.floor(minutes() / 60);
+    }
+
+    /**
+     * Used to check if the current seconds is on the time multiple of the given variable
+     * @param second What second, for instance checking on 5 will trigger every 5 seconds
+     * @return True if on time
+     */
+    public static boolean onSecond(int second) {
+        return seconds() % second == 0;
+    }
+
+    /**
+     * Used to check if the current minutes is on the time multiple of the given variable
+     * @param minute What minute, for instance checking on 5 will trigger every 5 minutes
+     * @return True if on time
+     */
+    public static boolean onMinute(int minute) {
+        return minutes() % minute == 0;
+    }
+
+    /**
+     * Used to check if the current hours is on the time multiple of the given variable
+     * @param hour What hour, for instance checking on 5 will trigger every 5 hours
+     * @return True if on time
+     */
+    public static boolean onHour(int hour) {
+        return hours() % hour == 0;
+    }
+
+    @SubscribeEvent
+    public void onWorldTick(TickEvent.WorldTickEvent worldTick) {
+        if(worldTick.phase == TickEvent.Phase.END)
+            tick = worldTick.world.getTotalWorldTime();
+    }
+}
