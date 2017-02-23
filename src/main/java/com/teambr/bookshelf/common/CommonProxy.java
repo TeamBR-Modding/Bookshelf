be917a7fc8b82efc4e8e7135f3d7ebe2487fbe2e
Paul Davis <pauldavis101@gmail.com>
2017-02-22 18:48:50 -0600

    Sided fix

--- src/main/java/com/teambr/bookshelf/common/CommonProxy.java
+++ src/main/java/com/teambr/bookshelf/common/CommonProxy.java
@@ -1,9 +1,16 @@
 package com.teambr.bookshelf.common;
 
+import com.teambr.bookshelf.common.tiles.Syncable;
+import com.teambr.bookshelf.network.PacketManager;
+import com.teambr.bookshelf.network.SyncableFieldPacket;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 
 /**
  * This file was created for Bookshelf - Java
@@ -33,4 +40,21 @@ public class CommonProxy {
      * Called on postInit
      */
     public void postInit(FMLPostInitializationEvent event) {}
+
+    public IMessage doSyncableMessage(boolean returnValue, int id, double value, BlockPos blockPosition, World world) {
+        if(world.getTileEntity(blockPosition) == null)
+            return null;
+        else if(!(world.getTileEntity(blockPosition) instanceof Syncable))
+            return null;
+
+        if(returnValue)
+            PacketManager.net.sendToAllAround(new SyncableFieldPacket(false, id,
+                            ((Syncable)world.getTileEntity(blockPosition)).getVariable(id), blockPosition),
+                    new NetworkRegistry.TargetPoint(world.provider.getDimension(),
+                            blockPosition.getX(), blockPosition.getY(), blockPosition.getZ(), 25));
+        else {
+            ((Syncable)world.getTileEntity(blockPosition)).setVariable(id, value);
+        }
+        return null;
+    }
 }
